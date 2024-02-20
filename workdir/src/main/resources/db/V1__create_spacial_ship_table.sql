887b77d9415e2dbab2534cb4c8104f6964266b55
Alejandro <alejandrofdez.dev@gmail.com>
2024-02-20 17:37:43 +0100

    Add inserts to first script

--- workdir/src/main/resources/db/V1__create_spacial_ship_table.sql
+++ workdir/src/main/resources/db/V1__create_spacial_ship_table.sql
@@ -7,3 +7,7 @@ CREATE TABLE spacial_ship
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+INSERT INTO spacial_ship (name, model, type) VALUES ('Millennium Falcon', 'YT-1300 light freighter', 'Light freighter');
+INSERT INTO spacial_ship (name, model, type) VALUES ('X-wing', 'T-65 X-wing starfighter', 'Starfighter');
+INSERT INTO spacial_ship (name, model, type) VALUES ('Imperial I-class Star Destroyer', 'Imperial I-class Star Destroyer', 'Star Destroyer');
